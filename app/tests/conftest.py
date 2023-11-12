ab5f60520493537ab92f4f4dc51faa501d828e4a
Matias Brignone <mnbrignone@gmail.com>
2023-11-12 16:51:55 -0300

    Improve conftest to create DB tables only once

--- app/tests/conftest.py
+++ app/tests/conftest.py
@@ -1,3 +1,4 @@
+import asyncio
 from typing import AsyncGenerator
 
 import pytest
@@ -5,42 +6,53 @@ from httpx import AsyncClient
 from sqlalchemy.ext.asyncio import AsyncConnection, AsyncSession
 
 from app.main import app
-from app.models.database import AsyncSessionLocal, Base, async_engine, get_session
+from app.models.database import Base, async_engine, get_session
 
 
-@pytest.fixture(autouse=True)
+@pytest.fixture(scope="session")
+def event_loop():
+    """
+    Custom session-scoped event loop fixture, created to be able to use the
+    db_connection fixture with a session scope.
+    """
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    yield loop
+    loop.close()
+
+
+@pytest.fixture(scope="session", autouse=True)
 async def db_connection() -> AsyncGenerator[AsyncConnection, None]:
     """
     Fixture to use a single DB connection for the whole testsuite.
     """
 
     # always drop and create test DB tables between test sessions
-    async with async_engine.begin() as connection:
+    async with async_engine.connect() as connection:
         await connection.run_sync(Base.metadata.drop_all)
         await connection.run_sync(Base.metadata.create_all)
+        await connection.commit()  # ensure any open transactions are committed
+
+        yield connection
 
 
-@pytest.fixture()
-async def db_session() -> AsyncGenerator[AsyncSession, None]:
+@pytest.fixture(scope="function")
+async def db_session(
+    db_connection: AsyncConnection
+) -> AsyncGenerator[AsyncSession, None]:
     """
     Fixture to create a new separate transaction for each testcase, rolling back all
     the DB changes after the test finishes.
     This ensures that each testcase starts with an empty database.
     """
 
-    # async with async_engine.connect() as connection:
-    #     transaction = await connection.begin()
-
-    #     async with AsyncSessionLocal(bind=connection) as session:
-    #         yield session
-
-    #     await transaction.rollback()
-
-    async with AsyncSessionLocal() as session:
+    async with AsyncSession(bind=db_connection, expire_on_commit=False) as session:
+        await session.begin_nested()  # create savepoint to rollback to
         yield session
+        await session.rollback()  # rollback to the savepoint
 
 
-@pytest.fixture()
+@pytest.fixture(scope="function")
 async def client(db_session) -> AsyncGenerator[AsyncClient, None]:
     # override get_session dependency to return the DB session from the fixture
     # (instead of creating a new one)
